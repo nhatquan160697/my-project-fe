bec4209d8bbc573ce8d2ebf52951179fa7ce3ccb
ton.that.nhat.quan <SUN-ASTERISK\ton.that.nhat.quan@b122024-pc.sun-asterisk.com>
2020-06-02 16:10:02 +0700

    Add template and css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+require('bootstrap/dist/css/bootstrap.css')
+require('devicons/css/devicons.css')
+require('font-awesome/css/font-awesome.css')
 
 Vue.config.productionTip = false
 
