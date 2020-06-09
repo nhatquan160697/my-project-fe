408934545bf24def600dff9daa2bdfdb0616cbf3
Quan Ton That Nhat (quanttn-2024) <ton.that.nhat.quan@sun-asterisk.com>
2020-06-09 10:09:18 +0700

    Adding js and component

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import jquery from 'jquery'
 import router from './router'
+require('bootstrap')
+require('jquery.easing')
 require('bootstrap/dist/css/bootstrap.css')
 require('devicons/css/devicons.css')
 require('font-awesome/css/font-awesome.css')
+
+Vue.prototype.jquery = jquery
 
 Vue.config.productionTip = false
 
